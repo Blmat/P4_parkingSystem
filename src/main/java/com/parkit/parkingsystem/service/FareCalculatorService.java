e6101e750645d10bd1e9e5406ef5e3de7d03abef
Mathieu Blanc <94637553+Blmat@users.noreply.github.com>
2022-03-30 20:02:23 +0200

    The project!

---
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -0,0 +1,42 @@
+package com.parkit.parkingsystem.service;
+
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.model.Ticket;
+
+import java.time.Duration;
+
+public class FareCalculatorService {
+
+    public void calculateFare(Ticket ticket) {
+        if ((ticket.getOutTime() == null)
+                || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+        }
+
+        Duration duration = Duration.between(ticket.getInTime().toInstant(),
+                ticket.getOutTime().toInstant());
+
+        if (duration.toMinutes() < 30) {
+            ticket.setPrice(0f);
+        } else {
+            // Duration en heure: 60f = conversion en double
+            switch (ticket.getParkingSpot().getParkingType()) {
+                case CAR: {
+                    ticket.setPrice(duration.toMinutes() / 60f * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
+                case BIKE: {
+                    ticket.setPrice(duration.toMinutes() / 60f * Fare.BIKE_RATE_PER_HOUR);
+                    break;
+                }
+                default:
+                    throw new IllegalArgumentException("Unkown Parking Type");
+            }
+        }
+    }
+    public void discountForRegularCustomer(Ticket ticket,double discount) {
+        calculateFare(ticket);
+        ticket.setPrice(ticket.getPrice()* (1-Math.ceil(discount)/100));
+
+    }
+}
